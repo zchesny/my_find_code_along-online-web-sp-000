9da5811307481c55850f344937986a24f44f485c
Zoey Chesny <z.chesny@gmail.com>
2020-04-09 21:38:01 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  while i < collection.length 
+  while i < collection.length
     return collection[i] if yield(collection[i])
-    i += 1 
+    i += 1
   end
 end
